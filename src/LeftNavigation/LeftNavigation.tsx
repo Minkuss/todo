1538b2ca1289ff90173174dc179f3e1c6daf9309
Nikita Shijan <nikusia0804@gmail.com>
2022-09-06 17:21:02 +1000

    axios

--- src/LeftNavigation/LeftNavigation.tsx
+++ src/LeftNavigation/LeftNavigation.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import axios from "axios";
 import { Button, ButtonGroup, Card } from "@blueprintjs/core";
 
 import * as classes from "./LeftNavigation.styles";
@@ -8,6 +9,16 @@ import { ITodo } from "../types";
 
 export const LeftNavigation: FC = () => {
   const [searchedTodos, setSearchedTodos] = useState<ITodo[]>([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const apiURL = "https://todo-project.up.railway.app/api/users";
+    axios.get(apiURL).then((resp) => {
+      const allUsers: [] = resp.data;
+      console.log(allUsers);
+      setUsers(allUsers);
+    });
+  }, [setUsers]);
 
   const onChange = (text: string) => {
     if (text !== "") {
@@ -29,7 +40,7 @@ export const LeftNavigation: FC = () => {
           {searchedTodos.map((el) => {
             return (
               <div
-                key = {el.id}
+                key={el.id}
                 style={{
                   display: "flex",
                 }}
@@ -93,6 +104,9 @@ export const LeftNavigation: FC = () => {
               </Button>
             )}
           </NavLink>
+          {users.map((el: { name: string }) => (
+            <div>{el.name}</div>
+          ))}
         </ButtonGroup>
       </Card>
     </>
